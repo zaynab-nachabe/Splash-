b65e966d329dc55336949331601d660b1e68f1f6
zaynab-nachabe <nachabezaynab5@gmail.com>
2025-03-29 17:23:21 +0100

    added a mock for child stats #40

--- frontend/src/app/pages/ergo/stat-selected-page/ergo-stat-selected-page.component.ts
+++ frontend/src/app/pages/ergo/stat-selected-page/ergo-stat-selected-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { gamestatMock } from '../../../shared/mocks/gamestat-mock';
 @Component({
   selector: 'app-ergo-stat-selected-page',
   templateUrl: './ergo-stat-selected-page.component.html',
   styleUrl: './ergo-stat-selected-page.component.scss'
 })
 export class ErgoStatSelectedPageComponent {
-
+  gamestat = gamestatMock;
 }
