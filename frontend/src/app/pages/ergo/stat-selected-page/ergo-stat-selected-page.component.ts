47d2aa2303c7448c6bf66c804185a75f3a8732fe
zaynab-nachabe <nachabezaynab5@gmail.com>
2025-03-31 11:24:08 +0200

    sorry fixed some of the imports #45

--- frontend/src/app/pages/ergo/stat-selected-page/ergo-stat-selected-page.component.ts
+++ frontend/src/app/pages/ergo/stat-selected-page/ergo-stat-selected-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { gamestatMock } from '../../../shared/mocks/gamestat-mock';
+import { playerGameStatMock } from '../../../shared/mocks/playerGamestat-mock';
 @Component({
   selector: 'app-ergo-stat-selected-page',
   templateUrl: './ergo-stat-selected-page.component.html',
   styleUrl: './ergo-stat-selected-page.component.scss'
 })
 export class ErgoStatSelectedPageComponent {
-  gamestat = gamestatMock;
+  gamestat = playerGameStatMock;
 }
--- frontend/src/app/shared/models/user.model.ts
+++ frontend/src/app/shared/models/user.model.ts
@@ -3,5 +3,4 @@ export type User = {
     name: string;
     age: string;
     icon: string;
-    user_id: number;
 }
