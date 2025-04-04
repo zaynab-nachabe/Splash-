3b87267170f0a6e976544ee5434b941cc64ecaa1
zaynab-nachabe <nachabezaynab5@gmail.com>
2025-04-04 22:58:59 +0200

    added a localStorageService for the child ranking mock #57

--- frontend/src/app/shared/mocks/childRanking-mock.ts
+++ frontend/src/app/shared/mocks/childRanking-mock.ts
@@ -1,3 +1,4 @@
+import { LocalStorageService } from "../services/localStorage.service";
 import { UserService } from "../services/user.service";
 //the weird (() => {}) is an IIFE which is a function that defines a function and invokes it immediately
 //I used it in this context because without it i would need to define a seperate function and call it explicitly
@@ -6,7 +7,8 @@ import { UserService } from "../services/user.service";
 export const childstatMock = (() => {
     //i thought maybe we could do a dictionnary where the key is the userID and the value is the rank
     //so the mock would need to fetch the user ID first then i put whatever number as value
-    const userService = new UserService();
+    const storageService = new LocalStorageService();
+    const userService = new UserService(storageService);
     const users = userService['users'];
     const mock: {[key: string]: number} = {};
     const ranks = Array.from({ length: users.length }, (_, i) => i + 1);
