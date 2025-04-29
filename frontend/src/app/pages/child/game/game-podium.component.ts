39f2a5059e1516ca381e7d4c2a1b07458cfcce24
zaynab-nachabe <nachabezaynab5@gmail.com>
2025-04-29 17:07:41 +0200

    removed the confetti

--- frontend/src/app/pages/child/game/game-podium.component.ts
+++ frontend/src/app/pages/child/game/game-podium.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {User} from 'src/app/shared/models/user.model';
 import { UserService } from 'src/app/shared/services/user.service';
-import * as confetti from 'canvas-confetti';
 
 @Component({
     selector: 'app-game-podium',
@@ -19,36 +18,5 @@ export class GamePodiumComponent implements OnInit{
         const state = this.route.snapshot.queryParams;
         this.user = JSON.parse(state['user'] || '{}');
         this.userScore = this.userService.getScore();
-        this.launchConfetti();
     }
-
-    private launchConfetti(): void {
-        const canvas = document.getElementById('confetti-canvas') as HTMLCanvasElement;
-        const confettiInstance = confetti.create(canvas, {resize: true});
-
-        const duration = 5 *1000;
-        const end = Date.now() + duration;
-
-        const frame = () => {
-            confettiInstance({
-                particleCount: 10,
-                angle: 60,
-                spread: 55,
-                origin: { x: 0 },
-            });
-            confettiInstance({
-                particleCount: 10,
-                angle: 120,
-                spread: 55,
-                origin: { x: 1 },
-            });
-
-            if (Date.now() < end) {
-                requestAnimationFrame(frame);
-            }
-        };
-
-        frame();
-    }
-
 }
